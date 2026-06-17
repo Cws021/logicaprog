cb4f694050e85c3f08c00a2193db6d71f050bea2
Cawa Santos de Souza Cardoso <cardosocawa28@gmail.com>
2026-06-17 09:17:25 -0300

    Primeiro commit - Aulas iniciais de logica

---
+++ l031q02.js
@@ -0,0 +1,24 @@
+//2) Elaborar um programa que pergunte o nome do usuário,
+//Declaração de variáveis
+let nome ;
+let num1, num2, num3, num4, soma;
+console.log("Informe o seu nome: ")
+//leia(nome)
+nome = "Rafael";
+
+console.log("Informe o primeiro valor inteiro:")
+num1 = 5;
+console.log("Informe o segundo valor Inteiro: ")
+num2 = 8;
+console.log("Informe o terceiro valor Inteiro: ")
+num3 = 10;
+console.log("Informe o quarto valor Inteiro: ")
+num4 = 12;
+
+soma = num1 + num2 + num3 + num4
+console.log("A soma dos 4 valores inseridos é ", soma);
+console.log("SOMA: ")
+
+mult = num1 * num2 + num3 + num4
+console.log(`A multiplicão dos 4 valores inseridos é ${mult}`)
+console.log(`MULTI: ${num1} x ${num2} x ${num3} x ${num4} = ${mult}`)
